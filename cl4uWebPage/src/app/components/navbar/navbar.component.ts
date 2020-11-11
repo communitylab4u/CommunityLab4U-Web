892bfde3d7b17b2f0c47392a33068fb5e20c6e0a
Matias <matias.iturrieta.hdz@gmail.com>
2020-11-11 01:50:36 -0300

    Pagina Web - Fase alpha

--- cl4uWebPage/src/app/components/navbar/navbar.component.ts
+++ cl4uWebPage/src/app/components/navbar/navbar.component.ts
@@ -7,9 +7,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function() {
+      var currentScrollPos = window.pageYOffset;
+      if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
+        document.getElementById("navbar").style.visibility = "visible";
+      } else {
+        document.getElementById("navbar").style.top = "-20vh";
+        document.getElementById("navbar").style.visibility = "hidden";
+      }
+      prevScrollpos = currentScrollPos;
+    }
+  }
+
 
   ngOnInit(): void {
   }
 
+  /* Global */
+  toTop(){
+    window.scrollTo(0,0);
+  }
+
+  /* Inicio */
+  toPurpose(){
+    document.getElementById("proposito").scrollIntoView();
+  }
+  toContact(){
+    document.getElementById("contacto").scrollIntoView();
+  }
+
+  /* Investigacion */
+  toInvApl(){
+    document.getElementById("investigacion-aplicada").scrollIntoView();
+  }
+  toVinMed(){
+    document.getElementById("vinculacion-con-el-medio").scrollIntoView();
+  }
+  toAcaIE(){
+    document.getElementById("academia-innovacion-emprendimiento").scrollIntoView();
+  }
+  toAcaDe(){
+    document.getElementById("academia-desarrolladores").scrollIntoView();
+  }
+  toAcaPro(){
+    document.getElementById("academia-programadores").scrollIntoView();
+  }
+  toEmp(){
+    document.getElementById("emprendimientos").scrollIntoView();
+  }
 }
