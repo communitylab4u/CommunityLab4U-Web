8953246dccd6efa81b979cf780eb1697a1ad22fa
Matias <matias.iturrieta.hdz@gmail.com>
2020-12-05 17:53:30 -0300

    arreglo enlaces

--- cl4uWebPage/src/app/app-routing.module.ts
+++ cl4uWebPage/src/app/app-routing.module.ts
@@ -6,12 +6,15 @@ import { InicioComponent } from './inicio/inicio.component';
 import { InvestigacionComponent } from './investigacion/investigacion.component';
 
 const routes: Routes = [
-  { path: '', component: InicioComponent},
-  { path: ':id', component: InicioComponent},
+  { path: 'inicio', component: InicioComponent},
+  { path: 'inicio/:id', component: InicioComponent},
   { path: 'investigacion', component: InvestigacionComponent},
   { path: 'investigacion/:id', component: InvestigacionComponent},
   { path: 'equipo', component: EquipoComponent},
   { path: 'galeria', component: GaleriaComponent},
+  { path: 'inicio', component: GaleriaComponent},
+  { path: '',   redirectTo: '/inicio', pathMatch: 'full' },
+  { path: '**', redirectTo: '/inicio'},
 ];
 
 @NgModule({
